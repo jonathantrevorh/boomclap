367ab5f476ba7934f8a64247b30d03abc3d068ad
Jonathan Hunsucker <jonathantrevorh@gmail.com>
2014-07-23 23:44:53 -0700

    Make a live amplitude graph

--- js/boomclap.js
+++ js/boomclap.js
@@ -59,6 +59,7 @@ function start() {
 
 function startPlayerUI() {
     loadTemplate('player');
+    hookups['dump-sample'].onclick = dumpAndClamp;
 }
 
 var setupWorker = (function () {
@@ -71,6 +72,10 @@ var setupWorker = (function () {
         }
     }
 })();
+
+var toolChain = {
+    spool: null,
+};
 
 function gotStream(stream) {
     startPlayerUI();
@@ -89,19 +94,118 @@ function gotStream(stream) {
 
     var input = wireUp([source, biquadFilter, gainNode]);
 
-    var spool = new SampleSpooler(input, audioContext, 250);
-    hookups["dump-sample"].onclick = function () {
-        var contents = spool.dumpContents();
-        console.log(contents);
-    }
-
+    toolChain.spool = new SampleSpooler(input, audioContext, 250);
     var detector = new HitDetector(input, audioContext);
-    detector.onhit = function () {
-        var clampedSample = clampSample(spool.dumpContents());
-        registerSound(clampedSample);
-    }
+    detector.onhit = dumpAndClamp;
 
-    wireUp([input, spool.processor, detector.inspector, audioContext.destination]);
+    wireUp([input, toolChain.spool.processor, detector.inspector, audioContext.destination]);
+}
+
+function dumpAndClamp() {
+    var contents = toolChain.spool.dumpContents();
+    clampSample(contents);
+}
+
+function clampSample(sample) {
+    loadTemplate('clamp');
+    hookups['save'].onclick = startPlayerUI;
+    drawSound(sample, hookups['amplitude-graph'], DrawingPartial.Amplitude);
+}
+
+function registerSound(clampedSound) {
+    ;
+}
+
+function drawSound(sample, canvas, pointDrawingPartial) {
+    var context = canvas.getContext('2d');
+    var width = canvas.width;
+    var samples = sample.reduce(flattenDeep, []);
+    var totalSamples = samples.length;
+    var maximum = 0.01;
+    var buckets = samples.reduce(bucket, new Array(width / 2));
+    var stepSize = width / buckets.length;
+    for (var i=0 ; i < buckets.length ; i++) {
+        var b = buckets[i];
+        if (!b) continue;
+        var avg = b.map(Math.abs).reduce(average, 0);
+        var details = pointDrawingPartial(avg, maximum);
+        drawRect(i, details.height, details.color);
+    }
+    function drawRect(x, height, color) {
+        var newHeight = height * canvas.height;
+        context.beginPath();
+        context.fillStyle = color || '#000000';
+        context.fillRect(x, canvas.height / 2 - newHeight, stepSize, newHeight * 2);
+        context.stroke();
+    }
+}
+
+var DrawingPartial = {
+    Amplitude: function (sample, maximum) {
+        return {
+            height: sample / maximum,
+            color: '#000000',
+        };
+    }
+};
+
+function bucket(previousValue, currentValue, index, array) {
+    var group = Math.floor(index / previousValue.length);
+    previousValue[group] = previousValue[group] || [];
+    previousValue[group].push(currentValue);
+    return previousValue;
+}
+
+function add(previousValue, currentValue) {
+    if (typeof previousValue != 'number') {
+        previousValue = previousValue.reduce(add, 0);
+    }
+    if (typeof currentValue != 'number') {
+        currentValue = currentValue.reduce(add, 0);
+    }
+    return previousValue + currentValue;
+};
+
+function max(previousValue, currentValue) {
+    return previousValue > currentValue ? previousValue : currentValue;
+}
+
+function count(previousValue, currentValue) {
+    return previousValue + 1;
+}
+
+function countDeep(previousValue, currentValue) {
+    var newValue = 1;
+    if (typeof currentValue !== 'number') {
+        if (currentValue.reduce) {
+            newValue = currentValue.reduce(countDeep, 0);
+        } else {
+            newValue = currentValue.length || 0;
+        }
+    }
+    return previousValue + newValue;
+}
+
+function average(previousValue, currentValue, index, array) {
+    return previousValue + currentValue / array.length;
+}
+
+function flatten(previousValue, currentValue) {
+    return previousValue.concat(currentValue);
+}
+
+function flattenDeep(previousValue, currentValue) {
+    var newValue = currentValue;
+    if (Array.isArray(currentValue)) {
+        newValue = currentValue.reduce(flattenDeep, []);
+    } else if (currentValue instanceof Float32Array) {
+        // specific case to handle audio api output, copy to regular array
+        newValue = new Array(currentValue.length);
+        for (var i=0 ; i < currentValue.length ; i++) {
+            newValue[i] = currentValue[i];
+        }
+    }
+    return previousValue.concat(newValue);
 }
 
 /**
@@ -170,7 +274,14 @@ HitDetector.prototype.startPumping = function () {
 HitDetector.prototype.pump = function () {
     this.inspector.getFloatFrequencyData(this.freqData);
     // if has hit and this.onhit
-    //  this.onhit();
+    //  this.triggerHit();
+    this.triggerHit();
+}
+HitDetector.prototype.triggerHit = function () {
+    if (!this.onhit) {
+        return;
+    }
+    this.onhit();
 }
 
 function wireUp(nodes) {
